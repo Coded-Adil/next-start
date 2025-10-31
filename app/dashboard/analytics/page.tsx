eeba370401dd7586613f26226d4c5e1b9aa478de
Coded-Adil <itsadil2022@gmail.com>
2025-10-31 16:38:43 +0600

    learning static routing in next js

---
+++ app/dashboard/analytics/page.tsx
@@ -0,0 +1,11 @@
+
+const Analytics = () => {
+    return (
+        <div>
+            <h1>Analytics Dashboard</h1>
+            <p>This is the analytics page.</p>
+        </div>
+    );
+};
+
+export default Analytics;
---
+++ components/hello.tsx
@@ -0,0 +1,11 @@
+'use client';
+const Hello = () => {
+    return (
+        console.log("Hello I am a client component"),
+        <div>
+            Hello
+        </div>
+    );
+};
+
+export default Hello;
