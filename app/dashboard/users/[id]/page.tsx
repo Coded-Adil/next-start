7fdbdf3ad5842741a03e9cfdf0e679246f143e1a
Coded-Adil <itsadil2022@gmail.com>
2025-10-31 17:05:19 +0600

    learned about dynamic routing in next js

--- app/dashboard/users/[id]/page.tsx
+++ app/dashboard/users/[id]/page.tsx
@@ -1,8 +1,11 @@
+// params is a Promise that resolves to an object with an id property of type string
+// To user params in a server component, we should await it first by using async function
 
-const UserDetails = () => {
+const UserDetails = async ({params }: {params: Promise <{id: string}>}) => {
+    const {id} = await params;
     return (
         <div>
-            UserDetails
+            <h1>Showing details for user #{id}</h1>
         </div>
     );
 };
